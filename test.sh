2da986ecd529d81ea5218f48d8f356ab5b4b8bff
Chia-Chu Chou <jeffreychou1991@gmail.com>
2019-01-21 12:10:10 -0800

    small file name mistake

--- test.sh
+++ test.sh
@@ -13,10 +13,18 @@ run_tests() {
     mv $testname Output
 }
 mkdir Output
-process_tests=("process_spawner" "process_spawner -i 100000")
-process_folders=("process_test" "process_test_i100000")
-thread_tests=("thread_spawner" "thread_spawner -i 100000")
-thread_folders=("thread_test" "thread_test_i100000")
+process_tests=("process_spawner" "process_spawner -i 100000" "process_spawner -i 250000" \
+"process_spawner -t 5" "process_spawner -t 5 -i 100000" "process_spawner -t 5 -i 250000" \
+"process_spawner -t 10" "process_spawner -t 10 -i 100000" "process_spawner -t 10 -i 250000")
+process_folders=("process_test" "process_test_i100000" "process_test_i250000" \
+ "process_test_t5" "process_test_t5_i100000" "process_test_t5_i250000" \
+"process_test_t10" "process_test_t10_i100000" "process_test_t10_i250000")
+thread_tests=("thread_spawner" "thread_spawner -i 100000" "thread_spawner -i 250000" \
+"thread_spawner -t 5" "thread_spawner -t 5 -i 100000" "thread_spawner -t 5 -i 250000" \
+"thread_spawner -t 10" "thread_spawner -t 10 -i 100000" "thread_spawner -t 10 -i 250000")
+thread_folders=("thread_test" "thread_test_i100000" "thread_test_i250000" \
+"thread_test_t5" "thread_test_t5_i100000" "thread_test_t5_i250000" \
+"thread_test_t10" "thread_test_t10_i100000" "thread_test_t10_i250000")
 len=${#process_tests[@]}
 for((j=0; j<$len;++j))
 do
